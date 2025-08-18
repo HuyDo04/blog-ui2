ddac997764c51dbab21dee230e308ddeac7b8632
HUYDO <huydo041203@gmail.com>
2025-08-18 22:48:58 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ["@fortawesome/react-fontawesome"]
   },
   css: {
     preprocessorOptions: {
